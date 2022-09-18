c4879f5ec42d9471e57030faab11372bde968b9c
newonelei@gmail.com <newonelei@gmail.com>
2022-09-18 20:45:48 +1000

    + add comment of transpose calculation

--- physics2d/physics2d/math/matrices.cpp
+++ physics2d/physics2d/math/matrices.cpp
@@ -1,12 +1,13 @@
 #include "matrices.h"
 
 namespace math {
-void Transpose(const float* srcMat, float* dstMat, int srcRows,
-                      int srcCols) {
+void Transpose(const float* srcMat, float* dstMat, int srcRows, int srcCols) {
   for (int i = 0; i < srcRows * srcCols; ++i) {
     int row = i / srcRows;
     int col = i % srcRows;
-    dstMat[i] = srcMat[srcCols * col + row];
+    dstMat[i] = srcMat[srcCols * col +
+                       row];  // transposed element index on i([srcCols * row +
+                              // col]) is j([srcCols * col + row])
   }
 }
 }  // namespace math
