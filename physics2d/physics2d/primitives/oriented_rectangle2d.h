79c583cc1389575b45a257f941e117a7e577ce1b
newonelei@gmail.com <newonelei@gmail.com>
2022-10-26 21:58:13 +1100

    oriented rectangle 2d

---
+++ physics2d/physics2d/primitives/oriented_rectangle2d.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "math/vector2d.h"
+#include "point2d.h"
+
+#ifndef _H_2D_ORIENTED_RECTANGLE_
+#define _H_2D_ORIENTED_RECTANGLE_
+
+namespace primitives {
+typedef struct OrientedRectangle2D {
+  Point2D position_;
+  math::Vector2D half_extents_;
+  float rotation_;
+
+  // default constructor : unit length rectangle at origin with no rotation
+  inline OrientedRectangle2D() : half_extents_(1.0f, 1.0f), rotation_(0.0f) {}
+
+  // a constructor that takes a center point and half-extents with no rotation
+  inline OrientedRectangle2D(const Point2D& p, const math::Vector2D& e)
+      : position_{p}, half_extents_{e}, rotation_{0.0f} {}
+
+  // full constructor
+  inline OrientedRectangle2D(const Point2D& p, const math::Vector2D& ext,
+                             float rot)
+      : position_{p}, half_extents_{ext}, rotation_{rot} {}
+} OrientedRectangle2D;
+}  // namespace primitives
+
+#endif  // _H_2D_ORIENTED_RECTANGLE_
